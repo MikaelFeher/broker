ca2245feb6588838428c0f78f1fc078bff7e107b
MikaelFeher <mikael.feher@gmail.com>
2016-10-18 20:07:37 +0200

    Adds additional styling and code refactoring

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+	CITIES = ['Skurup', 'Beddingestrand']
+	# scope :skurup, -> {where(city: 'Skurup')}
+	# scope :beddingestrand, ->{where(city: 'Beddingestrand')}
 end
