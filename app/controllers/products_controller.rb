5cf04b186317c6b200dfaa076875cc5816de40a3
MikaelFeher <mikael.feher@gmail.com>
2016-10-20 22:15:36 +0200

    Adds admin dashboard and before filter for crud actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_admin!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   
   def index
