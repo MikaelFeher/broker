b19c8c7d41cdd9d4b6356c75b7206572f94dff25
MikaelFeher <mikael.feher@gmail.com>
2016-10-20 12:49:26 +0200

    Fixes jumbotron image issue Some minor refactoring

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,4 @@ class Product < ApplicationRecord
 
 	belongs_to :category
 	
-	# CITIES = ['Skurup', 'Beddingestrand']
-	# scope :skurup, -> {where(city: 'Skurup')}
-	# scope :beddingestrand, ->{where(city: 'Beddingestrand')}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+    
   root 'products#index'
 
   resources :products
-
-  mount Admin::Engine, at: "admin"
 
   end
